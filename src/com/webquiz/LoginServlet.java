87b2c7155dec0e546ddf95d58dddbc7e40bf4d8f
Sifang Chu <chusifang@yahoo.com>
2013-05-27 21:42:38 -0500

    Version 0.1

--- src/com/webquiz/LoginServlet.java
+++ src/com/webquiz/LoginServlet.java
@@ -1,40 +1,80 @@
 package com.webquiz;
 
 import java.io.IOException;
+
 import java.io.PrintWriter;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.*;
+import javax.servlet.http.*;
 
 public class LoginServlet extends HttpServlet {
+	
+	public class StudentObj
+	{
+		String _userName;
+		StudentObj (String userNameInput) 
+		{
+			_userName = userNameInput;
+		};
+	};
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doPost(req, resp);
-    }
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doPost(req, resp);
+	}
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String username = req.getParameter("username");
-        String password = req.getParameter("password");
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String userNameString=req.getParameter("username");
+		String passwordString=req.getParameter("password");
+		
 
-        System.out.println("username: " + username);
-        System.out.println("password: " + password);
+		if (validLogin(userNameString, passwordString)) {
+			StudentObj studentObj = new StudentObj(userNameString);
+			HttpSession httpSession = req.getSession();
+			httpSession.setAttribute("studentObj", (Object)studentObj);
+			showLoginSuccessForm(resp, userNameString);
+		}
+		else
+		{
+			showLoginDenyForm(resp);
+		}
 
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
 
-        out.println("<html>");
-        out.println("<head><title>Web Quiz</title></head>");
-        out.println("<body>");
+	}
 
-        out.println("<h1>Form Elements</h1>");
-
-        out.println("username: " + username + "<br />");
-        out.println("password: " + password + "<br />");
-        out.println("</body></html>");
-        out.close();
-    }
+	private boolean validLogin(String userNameString, String passwordString)
+	{
+		if (userNameString.endsWith("1"))
+		    return true;
+		else 
+			return false;
+	}
+	
+	private void showLoginSuccessForm(HttpServletResponse resp, String userNameString) throws IOException
+	{
+		resp.setContentType("text/html");
+		PrintWriter out=resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		
+		out.println("Welcome " + userNameString + " to Web Quiz!");
+		out.println("</body></html>");
+		out.close();
+	}
+	
+	private void showLoginDenyForm(HttpServletResponse resp) throws IOException
+	{
+		resp.setContentType("text/html");
+		PrintWriter out=resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		
+		out.println("Sorry. Try again. ");
+		out.println("<a href=login.html>Click Me</a>");
+		out.println("</body></html>");
+		out.close();
+	}
 }
